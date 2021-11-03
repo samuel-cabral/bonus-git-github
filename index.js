394d482e57c6b4b40e3b3e413475aad9a1e6bb68
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-11-03 11:40:45 -0300

    chore: altera retorno da rota

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/test', (req, res) => {
-  return res.json({ hello: 'world' });
+  return res.json({ hello: 'world 2' });
 });
 
 app.listen(3333, () => console.log(`🚀 Server listening on port 3333`));
